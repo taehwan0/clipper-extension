f3541d5435a794cb287aad3d39db8c219943d3e0
buddy <jang_taehwan@naver.com>
2023-03-29 15:42:50 +0900

    feat: pageToHtml

--- action.ts
+++ action.ts
@@ -7,8 +7,41 @@ document.getElementById('button1')?.addEventListener('click', () => {
    });
 });
 
-// document.getElementById('button2')?.addEventListener('click', () => {
-//     const html = document.documentElement.innerText;
-//     const textarea: any = document.getElementById('textarea1');
-//     textarea.value = html;
-// })
+document.getElementById('button2')?.addEventListener('click', () => {
+    console.log('hit')
+
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        var currTab = tabs[0];
+        
+        let tabId = 0;
+
+        if (currTab && currTab.id) { // Sanity check
+          tabId = currTab.id;
+        }
+
+        const detail = {
+            tabId: tabId
+        }
+
+        chrome.pageCapture.saveAsMHTML(detail, (mhtml) => {
+            const textarea: any = document.getElementById('textarea1');
+            if (textarea) {
+                textarea.value = blobToString(mhtml);
+                console.log(mhtml);
+            }
+        })
+    });
+})
+
+function blobToString(b?: Blob) {
+    if (b) {
+        var u, x;
+        u = URL.createObjectURL(b);
+        x = new XMLHttpRequest();
+        x.open('GET', u, false); // although sync, you're not fetching over internet
+        x.send();
+        URL.revokeObjectURL(u);
+        return x.responseText;
+    }
+    return null;
+}
